92bfd2034468109985a85659e65f68507d168684
FranciscoGuedes <francisco.guedes@polarising.com>
2023-03-12 22:21:52 +0000

    contact view updated

--- src/templates/contact/contact.tsx
+++ src/templates/contact/contact.tsx
@@ -1,12 +1,16 @@
-import { IContactFields } from '../../../@types/generated/contentful';
+import { IContactSectionFields } from '../../../@types/generated/contentful';
+import AppButtonImage from '../../components/app-button-image/appButtonImage';
+import AppParagraph from '../../components/app-paragraph/appParagraph';
 import SocialMedia from '../../components/social-media/socialMedia';
 
 import { ContactModule } from '../../lib/interfaces/contentful/icontact';
+import { SocialMediaModule } from '../../lib/interfaces/contentful/isocialMedia';
+import { concatHttpsAndUrlFromContentful } from '../../utils/utility';
 
 import './contact.module.scss';
 
 interface ContactProps {
-  contactSectionProps: IContactFields[];
+  contactSectionProps: IContactSectionFields[];
   contactRef: any;
 }
 
@@ -19,27 +23,47 @@ const Contact = ({
   .next()
   .value;
 
-  const contactLinks: ContactModule.ISocialMediaLinks[] = contactData['socialMediaLinks'];
+  const statementParagraph = contactData['statement'][0]['fields']['paragraphContent']['content'];
+  const statementText: JSX.Element[] = statementParagraph.map((element: ContactModule.IContent, index: number) => {
+    let statementLabel = element['content'].map(text => {
+      return text.value
+    }).toString();
+    return (
+      <> 
+        <div key={index}>
+          <AppParagraph
+            className='contact-text-paragraph_title'
+          >
+            {statementLabel}
+          </AppParagraph>
+        </div>
+      </>
+    );
+  })
 
-  const titleSection: string = contactData.title;
+  const buttonDataFields: ContactModule.IFields2 = contactData['button']['fields'];
+  const buttonImageDataFields: ContactModule.IFile = contactData['buttonImage']['fields']['media']['fields']['file'];
+
+  const buttonImageUrl: string = concatHttpsAndUrlFromContentful(buttonImageDataFields.url);
+  const contactEmail: string = buttonDataFields.href;
 
   return (
     <>
       <section id="contact" className='contact-wrapper' ref={contactRef}>
-        <div className='title-wrapper'>
+        <div className='contact-title-wrapper'>
           <h1>
-            {titleSection}
+            {statementText}
           </h1>
-        </div>
-        <div className="contact-links">
-          <SocialMedia 
-            socialMediaProps={contactLinks}
-            isTitleOfContactActive
-            isSvgActive
-            isDescriptionSvgActive 
-            svgHeight={0} 
-            svgWidth={0}
-          />
+          <AppButtonImage
+              src={buttonImageUrl} 
+              imageWidth={136} 
+              imageHeight={76} 
+              classNameImage='contact-button-image' 
+              classNameButton="contact-button-button"
+              ariaLabel={''} 
+              href={`mailto:${contactEmail}`}
+              buttonLabel={buttonDataFields.title}       
+            />
         </div>
       </section>
     </>
