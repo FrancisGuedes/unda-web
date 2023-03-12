4a9c76c5d2b1d18c6b587b3bf42556d456e3f50b
FranciscoGuedes <francisco.guedes@polarising.com>
2023-03-12 22:18:55 +0000

    navbar updated

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,67 +1,81 @@
 import { NextPage } from 'next';
 import { useCallback, useEffect, useState } from 'react';
-import Link from 'next/link'
-import { IContactFields, INavbarFields } from '../../../@types/generated/contentful';
+import Link from 'next/link';
+import {
+  INavbarFields,
+} from '../../../@types/generated/contentful';
 
 import Logo from '../logo/logo';
 import AppLink from '../app-link/appLink';
 import Modal from '../modal/modal';
 import { LabelSocialMedia } from '../social-media/socialMedia';
 import { NavbarModule } from '../../lib/interfaces/contentful/inavbar';
-import { LogoModule } from '../../lib/interfaces/contentful/ilogo';
-import { ContactModule } from '../../lib/interfaces/contentful/icontact';
-import { getWindowSize, IWindowSize, useScrollYPosition } from '../../utils/utility';
+import {
+  getWindowSize,
+  IWindowSize,
+  useScrollYPosition,
+} from '../../utils/utility';
 import { strings } from '../../utils/strings';
 import MobileMenu from './mobile-menu/mobileMenu';
 import DesktopMenu from './desktop-menu/desktopMenu';
 import { NavSectionRefs } from '../../pages';
 
-
 import tabletSizeWindow from './navbar.module.scss';
 import './navbar.module.scss';
+import { SocialMediaModule } from '../../lib/interfaces/contentful/isocialMedia';
 
 interface INavbarProps {
   navbarSectionProps: INavbarFields[];
-  contactSectionProps: IContactFields[];
+  socialMediaData: SocialMediaModule.ISocialMediaContent[];
   handleModal: () => void;
   isModalActive: boolean;
   navSectionRefs: NavSectionRefs[];
 }
 
-const Navbar: NextPage<INavbarProps> = ({ 
+const Navbar: NextPage<INavbarProps> = ({
   navbarSectionProps,
-  contactSectionProps,
+  socialMediaData,
   handleModal,
   isModalActive,
-  navSectionRefs
+  navSectionRefs,
 }: INavbarProps) => {
-  const [contentfulNavbarData, setContentfulNavbarData] = useState<NavbarModule.INavbar>();
+  const [contentfulNavbarData, setContentfulNavbarData] =
+    useState<NavbarModule.INavbar>();
   const [isNavbarActive, setIsNavbarActive] = useState<boolean>(false);
   const [lastScrollY, setLastScrollY] = useState<number>(0);
   const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false);
-  const [windowSize, setWindowSize] = useState<IWindowSize | undefined>(getWindowSize());
+  const [windowSize, setWindowSize] = useState<IWindowSize | undefined>(
+    getWindowSize(),
+  );
   const [activeNavLink, setActiveNavLink] = useState<number>(0);
-  
+
   const scrollYPosition: number = useScrollYPosition();
-  
-  const windowWidthTablet: number = +tabletSizeWindow.tabletSizeWindow.slice(0, 3);
+
+  const windowWidthTablet: number = +tabletSizeWindow.tabletSizeWindow.slice(
+    0,
+    3,
+  );
   const windowWidth: number | undefined = windowSize?.innerWidth;
 
-  const logoData: LogoModule.ILogoData = new Map(Object.entries(navbarSectionProps))
-  .values()
-  .next()
-  .value['logoImage']['fields'];
+  // TODO add LOGO data here
+  const logoData: NavbarModule.ILogo = new Map(
+    Object.entries(navbarSectionProps),
+  )
+    .values()
+    .next().value['logo'];
 
-  const navbarData: NavbarModule.INavbar = new Map(Object.entries(navbarSectionProps))
-  .values()
-  .next()
-  .value;
+  const navbarData: NavbarModule.INavbar = new Map(
+    Object.entries(navbarSectionProps),
+  )
+    .values()
+    .next().value;
 
-  const navlinksData: NavbarModule.INavlink[] = new Map(Object.entries(navbarSectionProps))
-  .values()
-  .next()
-  .value['navlinks'];
-  
+  const navlinksData: NavbarModule.INavlink[] = new Map(
+    Object.entries(navbarSectionProps),
+  )
+    .values()
+    .next().value['navlinks'];
+
   useEffect(() => {
     setContentfulNavbarData(navbarData);
     toggleBodyOverflow();
@@ -76,23 +90,23 @@ const Navbar: NextPage<INavbarProps> = ({
     handleDomScrollActiveLink();
   }, [lastScrollY]);
 
-  const nearestIndex = (
+  /* const nearestIndex = (
     currentPosition: number,
     sectionPositionArray: any,
     startIndex: number,
-    endIndex: number
+    endIndex: number,
   ): number => {
-    if(sectionPositionArray !== null) {
+    if (sectionPositionArray !== null) {
       if (startIndex === endIndex) return startIndex;
       else if (startIndex === endIndex - 1) {
         if (
           Math.abs(
             sectionPositionArray[startIndex].headerRef.current.offsetTop -
-              currentPosition
+              currentPosition,
           ) <
           Math.abs(
             sectionPositionArray[endIndex].headerRef.current.offsetTop -
-              currentPosition
+              currentPosition,
           )
         )
           return startIndex;
@@ -124,9 +138,9 @@ const Navbar: NextPage<INavbarProps> = ({
         }
       }
     } else return 0;
-  };
+  }; */
 
-  const handleScrollActiveLink = () => {
+  /* const handleScrollActiveLink = () => {
     var index: number = nearestIndex(
       window.scrollY,
       navSectionRefs,
@@ -134,59 +148,75 @@ const Navbar: NextPage<INavbarProps> = ({
       navSectionRefs.length - 1
     );
     setActiveNavLink(index);
-  };
+  }; */
 
-  const renderNavlinks: JSX.Element[] = navlinksData.map((navlinkInfo: NavbarModule.INavlink, index: number) => {
-
-    return (
-        <li className={`${navlinkInfo.fields.name}-title`} key={navlinkInfo.sys.id}>
+  const renderNavlinks: JSX.Element[] = navlinksData.map(
+    (navlinkInfo: NavbarModule.INavlink, index: number) => {
+      //console.log("navlinkInfo: ", navlinkInfo)
+      return (
+        <li
+          className={`${navlinkInfo.fields.name}-title`}
+          key={index}
+        >
           <Link
-            rel={navlinkInfo.fields.rel} 
+            rel={navlinkInfo.fields.rel}
             className={`${navlinkInfo.fields.name}-link`}
             href={`#${navlinkInfo.fields.href}`}
           >
-              <a onClick={() => {
-                  windowWidth! <= windowWidthTablet ? handleMenuToggle() : '';
-                  // Modal will be triggered only on contact navlink
-                  if(navlinkInfo.fields.href === 'contact') {
-                    handleModal();
-                  };
-                }}
-                className={`underline ${activeNavLink === index ? "active" : "not-active"}`}
-              >
-                  {navlinkInfo.fields.title}
-              </a>
+            <a
+              onClick={() => {
+                windowWidth! <= windowWidthTablet ? handleMenuToggle() : '';
+                // Modal will be triggered only on contact navlink
+                if (navlinkInfo.fields.href === 'contact') {
+                  handleModal();
+                }
+              }}
+              className={`underline ${
+                activeNavLink === index ? 'active' : 'not-active'
+              }`}
+            >
+              {navlinkInfo.fields.title}
+            </a>
           </Link>
         </li>
-    );
-  })
+      );
+    },
+  );
 
   const controlNavbarOverflow = (): void => {
     if (typeof window !== 'undefined') {
-      if(window.scrollY > 250) { // set min heigth to hide
-        if (window.scrollY < lastScrollY) { // if scroll down, hide the navbar
-          setIsNavbarActive(false); 
-        } else { // if scroll up, show the navbar
-          setIsNavbarActive(true);  
+      if (window.scrollY > 250) {
+        // set min heigth to hide
+        if (window.scrollY < lastScrollY) {
+          // if scroll down, hide the navbar
+          setIsNavbarActive(false);
+        } else {
+          // if scroll up, show the navbar
+          setIsNavbarActive(true);
         }
       }
       // set current page location to use in the next move
-      setLastScrollY(window.scrollY); 
+      setLastScrollY(window.scrollY);
     }
   };
 
-  const handleMenuToggle = useCallback(() => setMobileNavOpen((mobileNavOpen: boolean) => !mobileNavOpen), []);
+  const handleMenuToggle = useCallback(
+    () => setMobileNavOpen((mobileNavOpen: boolean) => !mobileNavOpen),
+    [],
+  );
 
   const toggleBodyOverflow = (): void => {
-    mobileNavOpen ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'scroll';
-  }
+    mobileNavOpen
+      ? (document.body.style.overflow = 'hidden')
+      : (document.body.style.overflow = 'scroll');
+  };
 
   function handleWindowResize(): void {
     setWindowSize(getWindowSize());
   }
 
   function handleDomWindowResize(): (() => void) | undefined {
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
       window.addEventListener('resize', handleWindowResize);
       return () => {
         window.removeEventListener('resize', handleWindowResize);
@@ -206,72 +236,68 @@ const Navbar: NextPage<INavbarProps> = ({
 
   function handleDomScrollActiveLink(): (() => void) | undefined {
     if (typeof window !== 'undefined') {
-      document.addEventListener("scroll", handleScrollActiveLink);
+      //document.addEventListener('scroll', handleScrollActiveLink);
       return () => {
-        document.removeEventListener("scroll", handleScrollActiveLink);
+        //document.removeEventListener('scroll', handleScrollActiveLink);
       };
     }
   }
 
-  const contactData: ContactModule.ISocialMediaLinks[] = new Map(Object.entries(contactSectionProps))
-  .values()
-  .next()
-  .value['socialMediaLinks'];
-
   const classeSocialMedia: LabelSocialMedia = {
-    contactLink: "modal-contact-link", 
-    link: "modal-link", 
-    svgIcon: "modal-svg-icon"
+    contactLink: 'modal-contact-link',
+    link: 'modal-link',
+    svgIcon: 'modal-svg-icon',
   };
 
-  const labelModalContent = {...strings.component.navbar.modalContent};
+  const labelModalContent = { ...strings.component.navbar.modalContent };
+
+   //console.log('socialMediaData: ', socialMediaData);
 
   return (
-    <header className={`header-navbar-active ${isNavbarActive ? 'hidden' : ''} ${scrollYPosition > 0 ? 'shadow' : ''}`}>
-      <Logo 
-        logoImageProps={logoData} 
-        width={70} 
-        height={55} 
-        className='navbar-logo'
+    <header
+      className={`header-navbar-active ${isNavbarActive ? 'hidden' : ''} ${
+        scrollYPosition > 0 ? 'shadow' : ''
+      }`}
+    >
+      <Logo
+        logoImageProps={logoData}
+        width={70}
+        height={55}
+        className="navbar-logo"
       />
-      <MobileMenu 
-        menuToggle={handleMenuToggle} 
-        ariaLabel={contentfulNavbarData?.name} 
-        renderNavlinks={renderNavlinks} 
-        contactData={contactData} 
+      <MobileMenu
+        menuToggle={handleMenuToggle}
+        ariaLabel={contentfulNavbarData?.name}
+        renderNavlinks={renderNavlinks}
+        socialMediaContact={socialMediaData}
         isMobileMenuOpen={mobileNavOpen}
       />
-      <DesktopMenu 
-        renderNavlinks={renderNavlinks} 
-      />
+      <DesktopMenu renderNavlinks={renderNavlinks} />
       {/* BEGIN MODAL */}
-      { isModalActive
-          ?
-            <Modal
-              isModalBakgroundActive 
-              isSocialMediaActive 
-              classNameSocialMedia={classeSocialMedia}
-              socialMediaProps={contactData}
-              handleModal={handleModal}
-              svgWidthForSocial={16}
-              svgHeightForSocial={16}
+      {isModalActive ? (
+        <Modal
+          isModalBakgroundActive
+          isSocialMediaActive
+          classNameSocialMedia={classeSocialMedia}
+          socialMediaProps={socialMediaData}
+          handleModal={handleModal}
+          svgWidthForSocial={16}
+          svgHeightForSocial={16}
+        >
+          <div className="contact-modal-content">
+            <h1>{labelModalContent.title}</h1>
+            <AppLink
+              href={`mailto:${labelModalContent.mail}`}
+              className="huge-text"
             >
-              <div className="contact-modal-content">
-                <h1>{labelModalContent.title}</h1>
-                <AppLink 
-                  href={`mailto:${labelModalContent.mail}`}
-                  className='huge-text'
-                >
-                  <span>{labelModalContent.mail}</span>
-                </AppLink>
-              </div>
-            </Modal>
-          :
-            null
-      }
+              <span>{labelModalContent.mail}</span>
+            </AppLink>
+          </div>
+        </Modal>
+      ) : null}
       {/* END MODAL */}
     </header>
   );
-}
+};
 
 export default Navbar;
