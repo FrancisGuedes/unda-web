3ecabbd0f426dba09e8db6ec93052ccd91f10d3b
FranciscoGuedes <francisco.guedes@polarising.com>
2023-03-22 09:27:02 +0000

    custom hook for browser width

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { LabelCarouselClassName, LabelCarouselSlideTextClassName } from "../components/carousel/carousel";
+import { LabelCarouselClassName, LabelCarouselSlideTextClassName } from "../components/carousel.OLD/carousel";
 import { LabelSocialMedia } from "../components/social-media/socialMedia";
 import { HTTPS } from "../lib/endpoints";
 
@@ -8,23 +8,14 @@ export function createClassName(defaultClass: string, classes?: string): string 
 }
 
 export const checkUrlContentfulData = (
-  urlContentful: string | undefined | null, 
+  urlContentful: string | undefined | null,
   urlLocal: string
-  ): string => {
+): string => {
   /* validates a url from contentful, if it's not valid it will match the provided one */
-  if(urlContentful === undefined || urlContentful === null || !!urlContentful){
+  if (urlContentful === undefined || urlContentful === null || !!urlContentful) {
     urlContentful = urlLocal;
   }
   return urlContentful;
-}
-
-/* Does not work */
-export function errorHandler() {
-  process.on('uncaughtException', function (err) {
-    console.log(err); // Displays the Error;
-    console.error((new Date).toUTCString() + ' uncaughtException:', err.message); // Displays the Date and Error Message.
-    console.error(err.stack);  // Stack in Which the error occurred.
-  });
 }
 
 export function concatHttpsAndUrlFromContentful(httpsContentful: string): string {
@@ -32,29 +23,29 @@ export function concatHttpsAndUrlFromContentful(httpsContentful: string): string
 }
 
 export interface IWindowSize {
-  innerWidth: number, 
+  innerWidth: number,
   innerHeight: number
 }
 
 export function getWindowSize(): IWindowSize | undefined {
   if (typeof window !== "undefined") {
-    const {innerWidth, innerHeight} = window;
-    return {innerWidth, innerHeight};
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
   }
 }
 
 export function combineObjects<T extends LabelCarouselSlideTextClassName | LabelCarouselClassName | LabelSocialMedia>(
   defaultClass: T,
   classes: T | undefined
-  ): T {
-    if(classes) {
-      let merged = Object.entries(defaultClass).reduce((acc, [key, value]) => 
+): T {
+  if (classes) {
+    let merged = Object.entries(defaultClass).reduce((acc, [key, value]) =>
       // if key is already in defaultClass, add the values, otherwise, create new pair
       ({ ...acc, [key]: (acc[key] || 0) + ' ' + value }), { ...classes });
-      return merged;
-    } else {
-      return defaultClass;
-    }
+    return merged;
+  } else {
+    return defaultClass;
+  }
 }
 
 export const useScrollYPosition = () => {
@@ -63,7 +54,7 @@ export const useScrollYPosition = () => {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-        const updatePosition = () => {
+      const updatePosition = () => {
         setScrollPosition(window.pageYOffset)
       }
       //accounts for the scroll position when the component first mounts
@@ -76,4 +67,23 @@ export const useScrollYPosition = () => {
   }, [])
 
   return scrollPosition;
+}
+
+
+export const useWindowWidth = () => {
+  //listens for changes off the width of the browser window
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowWidth;
 }
